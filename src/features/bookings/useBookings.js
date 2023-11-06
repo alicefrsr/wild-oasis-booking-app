799fc5eddc0e0b3ddc7190273abadc659f3a62e1
alicefrsr <94556600+alicefrsr@users.noreply.github.com>
2023-11-06 22:16:16 +0100

    Implement prefetching on pagination / bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,8 +1,12 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { getBookings } from '../../services/apiBookings';
 import { useSearchParams } from 'react-router-dom';
+import { PAGE_SIZE } from '../../utils/constants';
 
 export function useBookings() {
+  // for pre-fetching data
+  const queryClient = useQueryClient();
+
   const [searchParams] = useSearchParams();
 
   // FILTER
@@ -26,6 +30,7 @@ export function useBookings() {
   // PAGINATION
   const page = !searchParams.get('page') ? 1 : Number(searchParams.get('page'));
 
+  // QUERY
   const {
     data: { data: bookings, count } = {},
     isLoading,
@@ -34,5 +39,23 @@ export function useBookings() {
     queryKey: ['bookings', filter, sortBy, page],
     queryFn: () => getBookings({ filter, sortBy, page }),
   });
+
+  // PRE-FETCHING
+  // NEXT PAGE (pass in same object as in useQuery, expect we want page + 1)
+  //// to prevent unnecessary fetching of next page when on last page:
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+  if (page < pageCount)
+    ////
+    queryClient.prefetchQuery({
+      queryKey: ['bookings', filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+  // PREVIOUS PAGE (pass in same object as in useQuery, expect we want page - 1)
+  if (page > 1)
+    queryClient.prefetchQuery({
+      queryKey: ['bookings', filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
+
   return { isLoading, error, bookings, count };
 }
