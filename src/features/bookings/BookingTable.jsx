e53abd4c3bef418ae2b426a26ce2dc4d4ea909ca
alicefrsr <94556600+alicefrsr@users.noreply.github.com>
2023-11-06 19:52:38 +0100

    Reusable pagination component

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -4,6 +4,7 @@ import Menus from '../../ui/Menus';
 import Empty from '../../ui/Empty';
 import Spinner from '../../ui/Spinner';
 import { useBookings } from './useBookings';
+import Pagination from '../../ui/Pagination';
 
 function BookingTable() {
   // const bookings = [];
@@ -30,6 +31,9 @@ function BookingTable() {
             <BookingRow key={booking.id} booking={booking} />
           )}
         />
+        <Table.Footer>
+          <Pagination numResults={10} />
+        </Table.Footer>
       </Table>
     </Menus>
   );
