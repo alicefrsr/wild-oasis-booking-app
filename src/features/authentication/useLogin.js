315b8129a58e04cdcfbc0301472b595c3c6c3a95
alicefrsr <94556600+alicefrsr@users.noreply.github.com>
2023-11-09 01:24:37 +0100

    Start implementing auth: user login

---
+++ src/features/authentication/useLogin.js
@@ -0,0 +1,26 @@
+import { useMutation } from '@tanstack/react-query';
+import { login as loginApi } from '../../services/apiAuth';
+import { useNavigate } from 'react-router-dom';
+import toast from 'react-hot-toast';
+
+function useLogin() {
+  const navigate = useNavigate();
+  const { mutate: login, isLoading } = useMutation({
+    mutationFn: ({ email, password }) =>
+      loginApi({
+        email,
+        password,
+      }),
+    onSuccess: (user) => {
+      console.log(user);
+      navigate('/dashboard');
+    },
+    onError: (err) => {
+      console.log('ERROR', err);
+      toast.error('Invalid login credentials');
+    },
+  });
+  return { login, isLoading };
+}
+
+export default useLogin;
