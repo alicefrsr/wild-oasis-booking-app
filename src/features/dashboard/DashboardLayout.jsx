07f6fb109fa9716801f8bfdf35d7c7c49280995d
alicefrsr <94556600+alicefrsr@users.noreply.github.com>
2023-11-11 20:32:43 +0100

    Implement a line chart

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -4,6 +4,7 @@ import { useRecentStays } from './useRecentStays';
 import { useCabins } from '../cabins/useCabins';
 import Spinner from '../../ui/Spinner';
 import Stats from './Stats';
+import SalesChart from './SalesChart';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -38,6 +39,7 @@ function DashboardLayout() {
       <div>Today's activity</div>
       <div>Chart stay durations</div>
       <div>Chart sales</div>
+      <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
 }
